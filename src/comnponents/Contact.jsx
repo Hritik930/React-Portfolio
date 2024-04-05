8d85f07e71d95199c3be8c6863c6c76d265effa7
Hritik <hritikblcm@gmail.com>
2024-04-05 13:30:35 +0530

    color and background change

--- src/comnponents/Contact.jsx
+++ src/comnponents/Contact.jsx
@@ -15,12 +15,12 @@ const Contact = () => {
         <h1><span>C</span>ONTACT-US</h1>
         <div className="contact-icon" data-aos="fade-up"
      data-aos-anchor-placement="top-bottom">
-          <a href='https://www.instagram.com/' target='_blank' className="items"><FaInstagram className='icons'/></a>
-          <a href='https://whatsapp.com/' target='_blank' className="items"><FaWhatsapp className='icons'/></a>
-          <a href='https://www.linkedin.com/in/sahil-kasaudhan-7558252a1' target='_blank' className="items"><FaLinkedin className='icons'/></a>
-          <a href='https://twitter.com/Sahilk930' target='_blank' className="items"><BsTwitterX className='icons'/></a>
-          <a href='https://github.com/Hritik930' target='_blank' className="items"><FaGithub className='icons'/></a>
-          <a href='mailto:hritikblcm@gmail.com' target='_blank' className="items"><SiGmail className='icons'/></a>
+          <a href='https://www.instagram.com/' target='_blank' className="items"><FaInstagram className='icons' style={{color:"#B52C5E"}}/></a>
+          <a href='https://whatsapp.com/' target='_blank' className="items"><FaWhatsapp className='icons' style={{color:"#508352"}}/></a>
+          <a href='https://www.linkedin.com/in/sahil-kasaudhan-7558252a1' target='_blank' className="items"><FaLinkedin className='icons' style={{color:"#275C98"}}/></a>
+          <a href='https://twitter.com/Sahilk930' target='_blank' className="items"><BsTwitterX className='icons' style={{color:"#C3C3C3"}}/></a>
+          <a href='https://github.com/Hritik930' target='_blank' className="items"><FaGithub className='icons' style={{color:"#607D8B"}}/></a>
+          <a href='mailto:hritikblcm@gmail.com' target='_blank' className="items"><SiGmail className='icons' style={{color:"#691818"}}/></a>
         </div>
       </div>
     </>
